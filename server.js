7cc9ca4fe1061617cc4f9b7f512b3ee8f0ff2dc2
prachimehta07 <prachimehta001312@gmail.com>
2026-08-19 00:40:09 +0530

    Enable CORS for React frontend integration

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 
+const cors = require("cors");
 const connectDB = require("./config/db");
 const logger = require("./middleware/logger");
 const validateContentType = require("./middleware/validateContentType");
@@ -12,6 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
 app.use(logger);
 app.use(validateContentType);
 
